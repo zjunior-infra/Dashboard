90baf6ca8109a5101d2cf93f50b37f15c6b9f663
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-04-26 11:16:53 +0200

    remove spaces

--- pages/api/jobs/confirm.ts
+++ pages/api/jobs/confirm.ts
@@ -9,11 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method not allowed' });
     }
-  
     const { selectedJobs } = req.body;
-
-
-  
 
     try {
       const confirmedJobs = await prisma.crawledJob.findMany({
