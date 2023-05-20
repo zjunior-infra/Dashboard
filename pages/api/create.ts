99c7707cfbccb075234db7c147eb1dd134db3506
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-05-20 19:48:27 +0300

    Revert "export edge prop"
    This reverts commit eb96828c2f631fda1120e2a15b3f73c0f32ce908.

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -3,7 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method not allowed' });
