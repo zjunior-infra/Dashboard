4b927db5d98c2f1898f739ffd0e5fa1154f86337
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-08-09 22:51:02 +0300

    update the prisma client, adding new endpoint

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -1,7 +1,7 @@
+import { prisma } from "@/lib";
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'POST') {
