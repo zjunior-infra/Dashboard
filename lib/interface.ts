a96b0123af9e14ed58c53a10e4d918126828c0dc
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-08-09 22:50:19 +0300

    use prisma once

---
+++ lib/interface.ts
@@ -0,0 +1,11 @@
+export interface Job {
+    id: string,
+    company: string
+    title: string
+    link: string
+    email: string
+    type: string
+    deadline: string
+    logo: string
+    skills: string
+  }
