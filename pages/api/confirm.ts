9fc1e4ff753fa8aac6eb82952cc3495d6280073d
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-08-18 17:48:24 +0300

    Optimized Job Confirmation Check

--- pages/api/confirm.ts
+++ pages/api/confirm.ts
@@ -4,41 +4,32 @@ import { prisma } from "@/lib";
 
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ message: 'Method not allowed' });
-    }
-    const { selectedJobs } = req.body;
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+  const { selectedJobs } = req.body;
 
-    try {
-      const confirmedJobs = await prisma.crawledOpportunity.findMany({
-        where: {
-          id: {
-            in: selectedJobs,
-          },
+  try {
+    // const newJobsInArhciveTABLE = await prisma.archivedJob.createMany({
+    // data : confirmedJobs,
+    // })
+
+    const newJobsInJOBTABLE = await prisma.opportunity.createMany({
+      data: selectedJobs,
+    })
+
+    const deletedJobsInCRAWLEDJOBTABLE = await prisma.crawledOpportunity.deleteMany({
+      where: {
+        id: {
+          in: selectedJobs,
         },
-      });
+      },
+    });
 
-      // const newJobsInArhciveTABLE = await prisma.archivedJob.createMany({
-        // data : confirmedJobs,
-      // })
-
-      const newJobsInJOBTABLE = await prisma.opportunity.createMany({
-        data : confirmedJobs,
-        })
-
-      const deletedJobsInCRAWLEDJOBTABLE = await prisma.crawledOpportunity.deleteMany({
-        where: {
-            id: {
-                in: selectedJobs,
-            },
-        },
-        });
-
-      res.status(200).json({ message: 'Jobs Confirmend' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    } 
+    res.status(200).json({ message: 'Jobs Confirmend' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 }
- 
-  
+
