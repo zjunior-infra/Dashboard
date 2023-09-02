071a8528ddb954aa9c9272d8595d808df406a0a7
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-09-02 04:48:56 +0300

    Implemented get using id

--- pages/api/opprtunities/[id].ts
+++ pages/api/opprtunities/[id].ts
@@ -2,19 +2,20 @@ import { prisma } from "@/lib";
 import { NextApiRequest, NextApiResponse } from "next";
 
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
-    const {id}:Partial<{ [key: string]: string | string[]; }> = req.query
-    try{
-        if(typeof id === 'string'){
-            const job:CrawledOpportunity = await prisma.crawledOpportunity.findUniqueOrThrow({
-                where:{
-                    id: id
-                }
-            });
-            return res.status(200).json(job)
-        }
-  }
-  catch(err){
-    return res.status(500).json({message:"there's smth went wrong",Error:err})
-  }
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const id = String(req.query.id);
+    try {
+        const job: CrawledOpportunity = await prisma.opportunity.findUniqueOrThrow({
+            where: {
+                id: id
+            }
+        });
+        res.status(200).json(job)
+    }
+    catch (err: unknown) {
+        if (err.code === 'P2025')
+            return res.status(404).json({ message: "There is no job with the provided id" });
+        else
+            return res.status(500).json({ message: "there's smth went wrong", Error: err })
+    }
 }
