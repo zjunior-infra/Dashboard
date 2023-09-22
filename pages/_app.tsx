e1d76aed642480fd54b4bc4a2fac0b1ca23d1f63
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-09-22 20:14:01 +0300

    merge main with kpi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider, createTheme } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const darkTheme = createTheme({
   palette: {
@@ -16,9 +18,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
   
       <ThemeProvider theme={darkTheme} >
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
         <Layout >
           <Component {...pageProps}  />
         </Layout>
+        </LocalizationProvider>
         <ToastContainer />
       </ThemeProvider>
     
---
+++ pages/kpis.tsx
@@ -0,0 +1,41 @@
+import { fetcher } from "@/lib/utils";
+import { Input } from "@mui/material";
+import { DateFieldProps, DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import useSWR, {mutate} from "swr";
+export default function Kpis(){
+    const {data, error, isLoading} = useSWR<any>('/api/kpi/recentAddedJobs?span=100',fetcher)
+    const [number , setNumber] = useState<any>()
+    const [span,setSpan] = useState<number>(0)
+    const [date,setDate] = useState('')
+
+    useEffect(()=>{
+        if(!isLoading)
+            setNumber(data.number)
+    },[data])
+
+    const onClick = (e)=>{
+        e.preventDefault()
+        
+    }
+    return (
+        <>
+    <Head>
+      <title>zJunior | KPIs</title>
+    </Head>
+
+    
+    <main className="flex justify-center gap-10 p-24">
+        <DatePicker disableFuture onChange={(value)=> console.log(value)}/>
+        <Input type="number" onChange={(value=>setSpan(value))} placeholder="Back in time x days"/>
+        <button onClick={onClick} className="p-2 px-10 border-2 border-slate-500 hover:bg-slate-500 duration-200 rounded-lg text-white">LookUp</button>
+    </main>
+    <section className="flex flex-col justify-center gap-10">
+            <h1 className="text-4xl font-bold text-center text-white"> Number of Jobs Pushed to Live</h1>
+            <h1 className="text-4xl font-bold text-center text-white">{number}</h1>
+        </section>
+    </>
+    )
+}
