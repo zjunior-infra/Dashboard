8491e99d7f9ef5f657ae39fff15a5335ca671a0e
Abdelrrahman Elhaddad <abdelrahmanelhaddad389@gmail.com>
2023-09-28 07:35:05 +0300

    Added GA Data API config

---
+++ lib/GA4.ts
@@ -0,0 +1,13 @@
+// Establishing the connections with the google analytics data API
+import BetaAnalyticsDataClient from "@google-analytics/data";
+
+const options = {
+    credentials: {
+        client_email: process.env.CLIENT_EMAIL,
+        private_key: process.env.PRIVATE_KEY
+    },
+    email: process.env.CLIENT_EMAIL,
+    projectId: process.env.PROJECT_ID
+}
+const analyticsDataClient = new BetaAnalyticsDataClient.BetaAnalyticsDataClient(options);
+export default analyticsDataClient;
